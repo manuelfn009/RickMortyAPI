2e0a03b42c5e8ac60c4d9de11566203490139e02
Manuel Fernández <manuelfn066@gmail.com>
2023-11-29 17:57:22 +0100

    update project

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
 let container = document.querySelector(".container");
+let select = document.getElementById("select");
 
-let url = "https://rickandmortyapi.com/api/character";
+select.addEventListener("change", selectAll);
+selectAll();
 
-fetch(url).then(response => response.json()).then(data => {
-
-    for (let i = 0; i < data.results.length; i++) {
-
+async function selectAll() {
+container.innerHTML = "";
+  let url = `https://rickandmortyapi.com/api/character${select.value}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.results.length; i++) {
         let card = document.createElement("div");
-        card.classList.add("card", "m-3", "p-3", "bg-dark", "text-white", "w-25", "h-100", "rounded-3", "text-center", "align-items-center", "justify-content-center", "cursor-pointer");
+        card.classList.add(
+          "card",
+          "m-3",
+          "p-3",
+          "text-white",
+          "w-25",
+          "h-75",
+          "rounded-3",
+          "cursor-pointer"
+        );
         container.appendChild(card);
         let img = document.createElement("img");
+        img.classList.add("rounded-circle");
         img.src = data.results[i].image;
         card.appendChild(img);
         let name = document.createElement("h2");
         name.textContent = data.results[i].name;
         card.appendChild(name);
-        let species = document.createElement("p");
-        species.textContent = "Specie: " + data.results[i].species;
-        card.appendChild(species);
-        let status = document.createElement("p");
-        status.textContent = data.results[i].status;
-        card.appendChild(status);
-        let gender = document.createElement("p");
-        gender.textContent = data.results[i].gender;
-        card.appendChild(gender);
+        let ul = document.createElement("ul");
+        card.appendChild(ul);
+        let species = document.createElement("li");
+        species.innerHTML = `<strong>Species: </strong> ${data.results[i].species}`;
+        ul.appendChild(species);
+        let status = document.createElement("li");
+        status.innerHTML = `<strong>Status: </strong> ${data.results[i].status}`;
+        ul.appendChild(status);
+        let gender = document.createElement("li");
+        gender.innerHTML = `<strong>Gender: </strong> ${data.results[i].gender}`;
+        ul.appendChild(gender);
         let origin = document.createElement("p");
-        origin.textContent = data.results[i].origin.name;
-        card.appendChild(origin);
-        let location = document.createElement("p");
-        location.textContent = data.results[i].location.name;
-        card.appendChild(location);
-    }
-})
-
+        origin.innerHTML = `<strong>Origin Name: </strong> ${data.results[i].origin.name}`;
+        ul.appendChild(origin);
+        let location = document.createElement("li");
+        location.innerHTML = `<strong>Location Name: </strong> ${data.results[i].location.name}`;
+        ul.appendChild(location);
+        switch (data.results[i].status) {
+          case "Alive":
+            card.classList.add("bg-success");
+            break;
+          case "Dead":
+            card.classList.add("bg-danger");
+            break;
+          case "unknown":
+            card.classList.add("bg-warning");
+            break;
+          default:
+            break;
+        }
+      }
+    });
+}
